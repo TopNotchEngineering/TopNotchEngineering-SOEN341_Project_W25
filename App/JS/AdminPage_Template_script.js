e6a8ca8c1a23f5d00e8b4f3ff931ac70117802c9
Funmatsu <a_honla@live.concordia.ca>
2025-02-13 15:54:19 -0500

    Added team creation and tweaked user creation with pop-up

--- App/JS/AdminPage_Template_script.js
+++ App/JS/AdminPage_Template_script.js
@@ -15,102 +15,156 @@ const profile_icon_popup = document.querySelector(".profile-icon");
 const popup = document.querySelector("#pop-up");
 profile_icon_popup.onclick = function(){
     popup.style.display = "block";
+    document.querySelector("#inside-pop-up").style.display = "block";
+    document.querySelector("#team-addition-form").style.display = "none";
 }
 const back = document.querySelector("#back");
 back.onclick = function() {
     popup.style.display = "none";
 }
-document.querySelector("#add-team").onclick = function() {
+document.querySelector("#back2").onclick = function() {
+    popup.style.display = "none";
+}
+document.querySelector("#add-teams").onclick = function() {
     popup.style.display = "block";
-    document.querySelector("#profile-pop-up").innerHTML = `
-                <div id="team-addition-form">
-                
-                <div id="TeamNameForm" >
-                    <label for="dropdown" id="NameInput-placeholder" style="left: 50px;">Team Name:</label>
-                        <input id="NameInput" placeholder="Name" style="padding-left: 45px;"><br><br>
-                    <label for="dropdown" id="RoleInput-placeholder" style="left: 50px;">Team Description:</label>
-                        <input id="team-desc" placeholder="Team description" style="padding-left: 50px;">
-                    <div class="general-buttons" id="create-team-button">
-                        Create Team
-                    </div>
-                    <a href="../HTML/AdminPage_Template.html" id="link">
-                        <div class="general-buttons" id="back" style = "top: -25px; position: relative">
-                            Back
-                        </div>
-                    </a>
-                    <!-- <a href="../HTML/AdminPage_Template.html">
-                        <div class="general-buttons" id="create-team-button">
-                            Create Team
-                        </div>
-                    </a> -->
-                </div>
-            </div>`;
+    document.querySelector("#inside-pop-up").style.display = "none";
+    document.querySelector("#team-addition-form").style.display = "block";
 }
 
-
-//Team create
-// const createTeam = document.querySelector("#create-team-button");
-// const teamcolumn = document.querySelector("#teams-column");
-// const Name = document.querySelector("#NameInput");
-// const teamdesc = document.querySelector("#team-desc");
-// const custombody = document.querySelector(".custom-body");
-// const teamForm = document.querySelector("#team-addition-form");
-
-// createTeam.onclick = generateDiv;
-// function generateDiv() {
-//     // Create a new div element
-//     var newDiv = document.createElement("div");
-
-//     // Add content to the new div
-//     newDiv.innerHTML = "This is a dynamically generated div";
-//     newDiv.style.backgroundColor["black"];
-//     // Add a class to the new div for styling
-//     newDiv.className = "generated-div";
-
-//     // Append the new div to the container
-//     // var container = document.getElementById("container");
-//     teamForm.appendChild(newDiv);
-// }
-
-
-function loadDiv() {
-    // Parse the URL query parameters
-    // var params = new URLSearchParams(window.location.search);
-    var team = document.createElement("div");
-    team.innerHTML = localStorage.getItem("message");
-    // Create a new div element and set its text content
-        team.id = "sharedDiv";
-        team.style = document.getElementById("team-profile").style;
-        // // Append the new div to the container
-        // var container = document.getElementById("teams-column");
-        
-    document.getElementByClassName("custom-body").appendChild(newDiv);
+document.getElementById("create-team-button").onclick = function() {
+    generateDiv();
 }
 
-document.getElementById("create-team-button").addEventListener("click", function() {
-        generateDiv();
-    // document.getElementByClassName("custom-body").innnerText += "<br>";
-});
+// else alert("It doesnt even exist yet bud!");
 function generateDiv() {
     // Create a new div element
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
+    let newerDiv = document.createElement("div");
+    let roundDiv = document.createElement('div');
 
     // Add content to the new div
-    newDiv.innerHTML = document.getElementById("NameInput").value;
+    newDiv.innerHTML = document.getElementById("NameInput").value[0];
+    newerDiv.innerHTML = document.getElementById("NameInput").value;
+    roundDiv.innerHTML = document.getElementById("NameInput").value[0];
+    
+    var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+
+        // Create the RGB string
+        var rgbColor = "rgb(" + r + "," + g + "," + b + ")";
     document.getElementById("NameInput").value = "";
-    alert("Working or not??");
-    newDiv.style.backgroundColor = "rgb(44, 121, 202)";
-    newDiv.style.width = "fit-content";
-    newDiv.style.textAlign = "right";
-    newDiv.style.position = "relative";
-    newDiv.style.borderRadius = "50px";
-    newDiv.style.padding = "15px";
-    newDiv.style.margin = "10px";``
-    // newDiv.style.float = "right";
-    // Add a class to the new div for styling
+    newDiv.setAttribute("style", `position: relative;
+        text-align: center;
+        top: 5px;
+        left: 12px;
+        width: 25px;
+        height: 25px;
+        background-color: white;
+        padding: 15px;
+        padding-top: 12px;
+        padding-bottom: 17px;
+        background-color: ${rgbColor};
+        border-radius: 50px;
+        margin-top: 10px;
+        margin-bottom: 20px;
+        cursor: pointer;
+        overflow: hidden;
+        font-size: 30px;`);
+    newDiv.addEventListener("mouseover", function() {
+        newDiv.setAttribute("style", 
+        `text-align: center;
+        position: relative;
+            top: 5px;
+            left: 6px;
+            width: 25px;
+            height: 25px;
+            background-color: white;
+            padding: 15px;
+            padding-top: 12px;
+            padding-bottom: 17px;
+            
+            border-radius: 50px;
+            margin-top: 10px;
+            margin-bottom: 20px;
+            cursor: pointer;
+            overflow: hidden;
+            font-size: 30px;
+        background-color: ${rgbColor};
+        padding: 22px;
+        padding-top: 18px;
+        padding-bottom: 24px;
+        `);
+    });
+    newDiv.addEventListener("mouseout", function() {
+        newDiv.setAttribute("style", `position: relative;
+            text-align: center;
+            top: 5px;
+            left: 12px;
+            width: 25px;
+            height: 25px;
+            background-color: white;
+            padding: 15px;
+            padding-top: 12px;
+            padding-bottom: 17px;
+            background-color: ${rgbColor};
+            border-radius: 50px;
+            margin-top: 10px;
+            margin-bottom: 20px;
+            cursor: pointer;
+            overflow: hidden;
+            font-size: 30px;`);
+    });
     newDiv.className = "generated";
 
     // Append the new div to the container
     // var container = document.getElementById("container");
-    document.querySelector(".custom-body").appendChild(newDiv);
+    document.querySelector("#teams-container").appendChild(newDiv);
+
+    newerDiv.setAttribute("style", 
+        `margin-top: 10px;
+        padding: 15px;
+        padding-left: 50px;
+        margin-bottom: 10px;
+        border-radius: 20px;`);
+
+        newerDiv.addEventListener("mouseover", function(){
+            newerDiv.setAttribute("style", 
+                `margin-top: 10px;
+                color: black;
+                background-color: rgb(44, 121, 202);
+                margin-bottom: 10px;
+                padding: 15px;
+                padding-left: 50px;
+                border-radius: 20px;`);
+        });
+
+        newerDiv.addEventListener("mouseout", function(){
+            newerDiv.setAttribute("style", 
+                `margin-top: 10px;
+                color: black;
+                margin-bottom: 10px;
+                padding: 15px;
+                padding-left: 50px;
+                border-radius: 20px;`);
+        });
+        
+        roundDiv.setAttribute("style", `
+            width: 30px;
+            text-align: center;
+            border-radius: 50px;
+            background-color: ${rgbColor};
+            padding-top: 3px;
+            padding-bottom: 3px;
+            left: -50px;
+            top: -35px;
+            position: relative;
+            margin: 10px;
+            `);
+        var n = 0;
+    newerDiv.id = "generatedNew";
+    document.getElementById("custom-team-container").appendChild(newerDiv);
+    newerDiv.appendChild(roundDiv);
+
 }
+
